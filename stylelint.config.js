60018950e16a005c92d37e2bbae4bb5fbaae60e8
Alexis Córdova <acordova@mac.com>
2017-11-03 17:41:54 -0700

    Update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  'extends': 'stylelint-config-standard',
+  'extends': [
+    'stylelint-config-standard'
+  ],
+  'plugins': [
+    'stylelint-scss'
+  ],
   'rules': {
-    'selector-pseudo-element-colon-notation': null
+    'selector-pseudo-element-colon-notation': null,
+    'number-no-trailing-zeros': null,
+    'at-rule-empty-line-before': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [true, {
+      'ignoreAtRules': ['/^import-/']
+    }]
   }
 }
